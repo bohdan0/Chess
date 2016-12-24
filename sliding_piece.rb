05f08cfbe2dae8e11e1a1b47b4dc873bbb931c2d
Bohdan Nakonechnyi <bnakonechnuy0@gmail.com>
2016-12-24 13:53:37 -0800

    refactor sliding piece

--- sliding_piece.rb
+++ sliding_piece.rb
@@ -1,30 +1,30 @@
 module SlidingPiece
-
   def moves
     result = []
     move_dirs.each do |dir|
       pos = add_pos(@pos, dir)
-      result.concat(rec_method(pos, dir))
+      result += rec_method(pos, dir)
     end
 
     result
   end
 
-  def rec_method(pos, dir)
-    return [] unless valid_move?(pos)
-    return [pos] if !@board[pos].is_a?(NullPiece) && @board[pos].color != @color
-    next_pos = add_pos(pos, dir)
-    # return [] if @board[pos].color != @color && @board[next_pos].color == @board[pos].color
-    [pos] + rec_method(next_pos, dir)
+  def rec_method(curr_pos, dir)
+    return [] unless valid_move(curr_pos)
+    return [curr_pos] unless @board[curr_pos].is_a?(NullPiece) ||
+                             @board[curr_pos].color == @color
+
+    next_pos = add_pos(curr_pos, dir)
+
+    [curr_pos] + rec_method(next_pos, dir)
+  end
+
+  def valid_move(pos)
+    @board.in_bounds?(pos) && @board[pos].color != @color
   end
 
   def add_pos(prev, dir)
     [prev[0] + dir[0], prev[1] + dir[1]]
-  end
-
-  def valid_move?(move)
-    @board.in_bounds?(move) &&
-    (@board[move].is_a?(NullPiece) || @board[move].color != @color)
   end
 
   def horizontal
